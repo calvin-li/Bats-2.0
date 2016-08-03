9e95e1436305e361a8758eb4f093b234beb7a478
calvin-li <calli@ucdavis.edu>
2016-08-03 00:20:27 -0700

    adjusted icons to look better as silhouettes. Might adjust further, also need to change notification click behavior

--- app/src/main/res/raw/svg.py
+++ app/src/main/res/raw/svg.py
@@ -1,5 +1,5 @@
 from os import system
-svg = r'<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="50px" height="50px" viewBox="-50 -80 100 100" xml:space="preserve">   <text      x="0" y="0" fill="white" stroke="COLOR" stroke-width="5px" font-size="85px" text-anchor="middle" font-weight="bold" font-family="monospace" transform="scale(XSCALE,1)">PERCENT</text></svg>'
+svg = r'<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="50px" height="50px" viewBox="-50 -80 100 100" xml:space="preserve">   <text      x="0" y="0" fill="white" stroke="COLOR" stroke-width="0px" font-size="85px" text-anchor="middle" font-weight="bold" font-family="monospace" transform="scale(XSCALE,1)">PERCENT</text></svg>'
 for i in range(0, 101):
 	color = "black"
 	if i < 20:
@@ -23,4 +23,4 @@ for i in range(0, 101):
 		f.write(out)
 	f.closed
 	png = "../mipmap-hdpi/a" + str(i) + ".png"
-	system("inkscape -f " + file + " -e " + png)
+	system("\"C:/Program Files/Inkscape/inkscape.exe\" -f " + file + " -e " + png)
