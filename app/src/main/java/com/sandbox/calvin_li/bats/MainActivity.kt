d4f32a91379eb87bb49b67861e06bf4192bca14f
Calvin Li <calvinliqh@gmail.com>
2024-02-18 17:11:20 -0800

    moved over old files, got something built

--- app/src/main/java/com/sandbox/calvin_li/bats/MainActivity.kt
+++ app/src/main/java/com/sandbox/calvin_li/bats/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.sandbox.calvin_li.bats
 
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +22,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.sandbox.calvin_li.bats.ui.theme.BatsTheme
 
 class MainActivity : ComponentActivity() {
+    private var mNotificationManager: NotificationManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,6 +36,53 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
+        }
+
+        //check if we should cancel notification instead of updating
+        if (intent.hasExtra("Action")) {
+            BatNotification.manager?.cancel(BatNotification.alarmPendingIntent!!)
+            BatNotification.alarmPendingIntent = null
+            mNotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager!!.cancel(BatNotification.notificationID)
+        } else if (BatNotification.alarmPendingIntent == null) {
+            BatNotification.displayNotification(this)
+
+            val alarmIntent = Intent(applicationContext, BatNotification::class.java)
+            BatNotification.alarmPendingIntent = PendingIntent.getBroadcast(applicationContext, 0, alarmIntent,
+                PendingIntent.FLAG_IMMUTABLE)
+
+            BatNotification.manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            BatNotification.manager!!.setRepeating(
+                AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(),
+                BatNotification.updateInterval.toLong(), BatNotification.alarmPendingIntent!!)
+            MainActivity.Companion.makeToast("Bats started", this)
+        }
+
+        finish()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    companion object {
+        fun makeToast(message: String, context: Context) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
         }
     }
 }
